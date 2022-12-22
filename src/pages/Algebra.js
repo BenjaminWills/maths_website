69b8c44ef38874f15697e9e0c8995d3d78faede4
Benjaminwills <benjaminwills047@gmail.com>
2022-12-22 22:21:38 +0000

    filled with random code

---
+++ src/pages/Algebra.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Algebra() {
+  return <div>Algebra</div>;
+}
---
+++ src/pages/Geometry.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Geometry() {
+  return <div>Geometry</div>;
+}
