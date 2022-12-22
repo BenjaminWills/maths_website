79f26725e0a4f4a19d59864d77320a44fd7ddbd5
Benjaminwills <benjaminwills047@gmail.com>
2022-12-22 20:40:44 +0000

    added unique message ids

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import uuidv4 from 'uuidv4';
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name == '') return null
+    setTodos(prevTodos => {
+      return [ ... prevTodos, {id:uuidv4(),name:name,complete:false}]
+    })
+    todoNameRef.current.value = null // clears input
+  }
+
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button> Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}> Add Todo</button>
       <button>clear complete</button>
       <div>0 todos left</div>
     </>
