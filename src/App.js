522312bf66be9623b1ff8fe2ed794ee408ba494a
Benjaminwills <benjaminwills047@gmail.com>
2022-12-22 22:13:10 +0000

    finished navbar (for now, look into button activa)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useState, useRef, useEffect } from 'react';
-import TodoList from './Components/TodoList';
-import { v4 as uuidv4 } from 'uuid';
-import Navbar from './Components/Navbar';
+import React, { useState, useRef, useEffect } from "react";
+import TodoList from "./Components/TodoList";
+import { v4 as uuidv4 } from "uuid";
+import Navbar from "./Components/Navbar";
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos' // storing the changes locally
+const LOCAL_STORAGE_KEY = "todoApp.todos"; // storing the changes locally
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
   useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    setTodos(storedTodos)
-  },[]) // Loading from local storage once upon refresh
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    setTodos(storedTodos);
+  }, []); // Loading from local storage once upon refresh
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
-  },[todos]) // Saving to local storage
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]); // Saving to local storage
 
-  function toggleTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
   }
 
   function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name == '') return null
-    setTodos(prevTodos => {
-      return [ ... prevTodos, {id:uuidv4(),name:name,complete:false}]
-    })
-    todoNameRef.current.value = null // clears input
+    const name = todoNameRef.current.value;
+    if (name == "") return null;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null; // clears input
   }
 
-  function handleClearTodos(){
-    const newTodos = todos.filter(todos => !todos.complete)
-    setTodos(newTodos)
+  function handleClearTodos() {
+    const newTodos = todos.filter((todos) => !todos.complete);
+    setTodos(newTodos);
   }
 
   return (
@@ -45,14 +45,14 @@ function App() {
         <Navbar />
       </React.Fragment>
       <div>
-        <TodoList todos={todos} toggleTodo = {toggleTodo}/>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
         <input ref={todoNameRef} type="text" />
         <button onClick={handleAddTodo}> Add Todo</button>
         <button onClick={handleClearTodos}>clear complete</button>
-        <div>{todos.filter(todo => !todo.complete).length}</div>
+        <div>{todos.filter((todo) => !todo.complete).length}</div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
+import "../Styles/main.css";
 
 export default function Navbar() {
   const navRef = useRef();
 
   function showNavBar() {
-    navRef.current.classList.toggle("responsive_nav");
+    // navRef.current.classList.toggle("responsive_nav");
+    if (!navRef.current) console.log(navRef.current.value);
   }
   return (
     <header>
-      <h3>Logo</h3>
+      <h3 href="/">Bens math project</h3>
       <nav>
         <a href="/">Home</a>
-        <a href="/">Algebra</a>
-        <a href="/">Calculus</a>
-        <a href="/">Geometry</a>
+        <a href="/subject/algebra">Algebra</a>
+        <a href="/subject/calculus">Calculus</a>
+        <a href="/subject/geometry">Geometry</a>
         <button className="nav-btn nav-close-btn" onClick={showNavBar}>
           <FaTimes />
         </button>
