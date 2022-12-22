2f23b43fd1150f0282171acd2452758807514307
Benjaminwills <benjaminwills047@gmail.com>
2022-12-22 21:26:44 +0000

    adding button toggles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 export default function Navbar() {
+  const navRef = useRef();
+
+  function showNavBar() {
+    navRef.current.classList.toggle("responsive_nav");
+  }
   return (
-    null
-  )
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">Algebra</a>
+        <a href="/">Calculus</a>
+        <a href="/">Geometry</a>
+        <button onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
