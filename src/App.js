3cdd1ff75c29081f8c50111d93b1dc1e51da02af
Benjaminwills <benjaminwills047@gmail.com>
2022-12-24 11:45:00 +0000

    adding routing to website

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
 import React, { useState, useRef, useEffect } from "react";
-import TodoList from "./Components/TodoList";
-import { v4 as uuidv4 } from "uuid";
 import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
 import Algebra from "./pages/Algebra";
 import Geometry from "./pages/Geometry";
 import Calculus from "./pages/Calculus";
+import { Route, Link } from "react-router-dom";
 
 const LOCAL_STORAGE_KEY = "todoApp.todos"; // storing the changes locally
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef();
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    setTodos(storedTodos);
-  }, []); // Loading from local storage once upon refresh
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]); // Saving to local storage
-
-  function toggleTodo(id) {
-    const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id === id);
-    todo.complete = !todo.complete;
-    setTodos(newTodos);
-  }
-
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value;
-    if (name == "") return null;
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
-    });
-    todoNameRef.current.value = null; // clears input
-  }
-
-  function handleClearTodos() {
-    const newTodos = todos.filter((todos) => !todos.complete);
-    setTodos(newTodos);
-  }
-
   return (
     <>
       <React.Fragment>
         <Navbar />
       </React.Fragment>
-      <div>
-        <TodoList todos={todos} toggleTodo={toggleTodo} />
-        <input ref={todoNameRef} type="text" />
-        <button onClick={handleAddTodo}> Add Todo</button>
-        <button onClick={handleClearTodos}>clear complete</button>
-        <div>{todos.filter((todo) => !todo.complete).length}</div>
-      </div>
-      <div>
-        <Algebra />
-        <Calculus />
-        <Geometry />
-      </div>
+
+      {/* <Route exact to="/" component={Home} />
+      <Route exact to="/algebra" component={Algebra} />
+      <Route exact to="/geometry" component={Geometry} />
+      <Route exact to="/calculus" component={Calculus} /> */}
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,10 +6,6 @@ import "../Styles/main.css";
 export default function Navbar() {
   const navRef = useRef();
 
-  function showNavBar() {
-    // navRef.current.classList.toggle("responsive_nav");
-    if (!navRef.current) console.log(navRef.current.value);
-  }
   return (
     <header>
       <h3 href="/">Bens math project</h3>
@@ -18,11 +14,11 @@ export default function Navbar() {
         <a href="/algebra">Algebra</a>
         <a href="/calculus">Calculus</a>
         <a href="/geometry">Geometry</a>
-        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+        <button className="nav-btn nav-close-btn">
           <FaTimes />
         </button>
       </nav>
-      <button className="nav-btn" onClick={showNavBar}>
+      <button className="nav-btn">
         <FaBars />
       </button>
     </header>
