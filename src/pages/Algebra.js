51aa209cde42ec376e41edcf00b635b34533aa69
Benjaminwills <benjaminwills047@gmail.com>
2022-12-24 15:48:13 +0000

    updated algebra

--- src/pages/Algebra.js
+++ src/pages/Algebra.js
@@ -1,9 +1,19 @@
 import React from "react";
+var Latex = require("react-latex");
 
 export default function Algebra() {
   return (
     <>
-      <div>Algebra</div>
+      <div>
+        <h1 className="boldText"> ALEGBRA</h1>
+        <div id="textBox">
+          <p>
+            Welcome to the wonderfully wacky world of algebra! This is the first
+            fundamental building block of maths. It may seem boring - but it is
+            essential for us to get to the cool stuff!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
