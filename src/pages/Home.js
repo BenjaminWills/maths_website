37f845a91794de09d397d4fdf235f3e57f87c6b7
Benjaminwills <benjaminwills047@gmail.com>
2022-12-24 12:55:47 +0000

    added text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 var Latex = require("react-latex");
 
 export default function Home() {
-  const fraction = `$$5 \\times 2 = 10$$`;
   return (
     <>
       <div>
@@ -16,7 +15,6 @@ export default function Home() {
           it and with this website I hope that you will too. Maths is
           deceptively simple, you just need to learn it in a way that suits you.
         </p>
-        <Latex>{fraction}</Latex>
       </div>
     </>
   );
