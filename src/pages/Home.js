3e0b211464de6ac17a1acba7e18354e734d6544c
Benjaminwills <benjaminwills047@gmail.com>
2022-12-24 12:35:36 +0000

    added texbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Navbar from "../Components/Navbar";
 
 export default function Home() {
   return (
     <>
       <div>
         <h1 className="boldText"> Welcome to my maths website!</h1>
+        <br />
+      </div>
+      <div id="textBox">
+        <h2>hello</h2>
       </div>
     </>
   );
