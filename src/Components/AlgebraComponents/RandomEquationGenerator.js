98f3c24fb0228a277ceed229383338e4c17a6e90
Benjaminwills <benjaminwills047@gmail.com>
2022-12-26 17:55:19 +0000

    oops

--- src/Components/AlgebraComponents/RandomEquationGenerator.js
+++ src/Components/AlgebraComponents/RandomEquationGenerator.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 export default function RandomEquationGenerator() {
-  function sayHello() {
-    alert("You clicked me!");
+  function generateEquation() {
+    return Math.floor(Math.random() * 10);
   }
   return (
-    <div>
-      <button type="button" onClick={sayHello}>
-        generate equation
-      </button>
-    </div>
+    <>
+      <div>
+        <h3>
+          x - {generateEquation()}
+          <button type="button" onClick={generateEquation}>
+            generate equation
+          </button>
+        </h3>
+      </div>
+      <div>
+        <input type="text" name="name" />
+        <button type="button">submit answer</button>
+      </div>
+    </>
   );
 }
