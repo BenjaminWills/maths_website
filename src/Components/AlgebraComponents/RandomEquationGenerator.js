1cccc3daab87ba9a568087bfc6218fa2c86f42cf
Benjaminwills <benjaminwills047@gmail.com>
2022-12-26 18:24:35 +0000

    successfully implemented simple random equation

--- src/Components/AlgebraComponents/RandomEquationGenerator.js
+++ src/Components/AlgebraComponents/RandomEquationGenerator.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 
 export default function RandomEquationGenerator() {
-  const [number, setNumber] = useState(5);
+  const [number1, setNumber1] = useState(5);
+  const [number2, setNumber2] = useState(9);
 
   function generateEquation() {
-    setNumber((prevNum) => Math.floor(Math.random() * 10));
+    setNumber1((prevNum) => Math.floor(Math.random() * 10));
+    setNumber2((prevNum) => Math.floor(Math.random() * 10));
   }
 
-  function isCorrectAnswer(submission) {
-    if (submission === number) {
-      return true;
-    }
-    return false;
+  function getSolution() {
+    return number1 + number2;
+  }
+
+  function showSolution() {
+    alert(`x is: ${getSolution()}`);
   }
 
   return (
     <>
       <div>
         <h3>
-          x - {number}
+          x - {number1} = {number2} {`\t \t \t `}
           <button type="button" onClick={generateEquation}>
             generate equation
           </button>
         </h3>
       </div>
-      <div>
-        <input type="text" name="name" />
-        <button type="button" onClick={isCorrectAnswer}>
-          submit answer
-        </button>
-      </div>
+      <button type="button" onClick={showSolution}>
+        <span>What is x? {`\t`}</span>
+      </button>
     </>
   );
 }
